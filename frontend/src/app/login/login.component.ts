4f68331524a01a01f5c4858b63d643074947c546
Miguel Pato <migu3l.pach3co@gmail.com>
2023-04-24 03:57:37 +0100

    im going to sleep

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { User } from '../types/user';
 import { HttpClient } from '@angular/common/http';
+import { Observable, fromEvent, timer } from 'rxjs';
+import { debounce, map } from 'rxjs/operators';
 
 
 @Component({
@@ -12,7 +14,42 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
 
+  isPasswordValid = false;
+  passwordCriteria: string[] = [];
+  criteria: Observable<string[]> = new Observable<string[]>();
+
   onSubmit() {
     // do something with the form data
+  }
+
+  verifyPassword(password: string) {
+    const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
+    const isPasswordValid = regex.test(password);
+
+    if (!isPasswordValid) {
+      const passwordCriteria: string[] = [];
+      if (password.length < 8) {
+        passwordCriteria.push('Password must be at least 8 characters long.');
+      }
+      if (!password.match(/[A-Z]/)) {
+        passwordCriteria.push('Password must contain at least one capital letter.');
+      }
+      if (!password.match(/[a-z]/)) {
+        passwordCriteria.push('Password must contain at least one small letter.');
+      }
+      if (!password.match(/[0-9]/)) {
+        passwordCriteria.push('Password must contain at least one number.');
+      }
+
+      this.criteria = new Observable<number>(observer => {
+        observer.next(0);
+      }).pipe(
+        map(() => passwordCriteria)
+      );
+    } else {
+      this.criteria = new Observable<string[]>(observer => {
+        observer.next([]);
+      });
+    }
   }
 }
