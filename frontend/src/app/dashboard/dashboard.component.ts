e337560411981c6161f83a099d6b00d69d0dec26
Renato Custodio <renatommc2002@gmail.com>
2023-05-02 23:22:58 +0100

    li

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
 
+  getListas(){
+    
+  }
 }
