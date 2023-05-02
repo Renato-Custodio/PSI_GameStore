0ff686f4250e98a66b20e35c3c61bd598b639690
fc56369 <pedromrp1@gmail.com>
2023-05-02 17:17:43 +0100

    update cenas

--- frontend/src/app/auth/auth.component.ts
+++ frontend/src/app/auth/auth.component.ts
@@ -1,42 +1,46 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
-const bcrypt = require('bcryptjs');
+import bcrypt from 'bcryptjs';
 import { Observable, fromEvent, timer } from 'rxjs';
 import { debounce, map } from 'rxjs/operators';
 import { User } from '../user';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent {
   username: string = '';
   password: string = '';
 
-  constructor(private authService: AuthService, private router : Router) {}
-  
+  constructor(private authService: AuthService, private router: Router) {}
+
   isPasswordValid = false;
   passwordCriteria: string[] = [];
   criteria: Observable<string[]> = new Observable<string[]>();
   user: User | null = null;
 
   onSubmit() {
-    if(this.user != null){
+    if (this.user != null) {
       this.router.navigate(['/dashboard']);
     }
   }
 
   authenticate(username: string, password: string): void {
     username = username.trim();
-    if (!username){
+    if (!username) {
       return;
     }
-    this.authService.authenticateUser(username, bcrypt.hash(password, 10)).subscribe(u => {console.log(u); this.user = u});
+    this.authService
+      .authenticateUser(username, bcrypt.hash(password, 10))
+      .subscribe((u) => {
+        console.log(u);
+        this.user = u;
+      });
   }
 
-  
   verifyPassword(password: string) {
     const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
     const isPasswordValid = regex.test(password);
@@ -47,26 +51,26 @@ export class AuthComponent {
         passwordCriteria.push('Password must be at least 8 characters long.');
       }
       if (!password.match(/[A-Z]/)) {
-        passwordCriteria.push('Password must contain at least one capital letter.');
+        passwordCriteria.push(
+          'Password must contain at least one capital letter.'
+        );
       }
       if (!password.match(/[a-z]/)) {
-        passwordCriteria.push('Password must contain at least one small letter.');
+        passwordCriteria.push(
+          'Password must contain at least one small letter.'
+        );
       }
       if (!password.match(/[0-9]/)) {
         passwordCriteria.push('Password must contain at least one number.');
       }
 
-      this.criteria = new Observable<number>(observer => {
+      this.criteria = new Observable<number>((observer) => {
         observer.next(0);
-      }).pipe(
-        map(() => passwordCriteria)
-      );
+      }).pipe(map(() => passwordCriteria));
     } else {
-      this.criteria = new Observable<string[]>(observer => {
+      this.criteria = new Observable<string[]>((observer) => {
         observer.next([]);
       });
     }
   }
 }
-
-
