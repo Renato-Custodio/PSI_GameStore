f1254c1f216c1a049cd896a0849228d8026b81ff
corrreia <tomasc2002@gmail.com>
2023-05-03 00:16:20 +0100

    added new fields to users

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,9 +1,57 @@
 import * as mongoose from "mongoose";
 
-interface IUser extends mongoose.Document {
-	username: string;
-	passwordHash: string;
+export interface IList {
+	name: string;
+	items: number[];
 }
+
+export interface IUserData {
+	avatar: Buffer;
+	games: number[];
+	lists: IList[];
+	following: string[];
+	followers: string[];
+}
+
+interface IUser extends mongoose.Document {
+	_id: string;
+	passwordHash: string;
+	userData: IUserData;
+}
+
+const listSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	items: {
+		type: [Number],
+		required: true,
+	},
+});
+
+const userDataSchema = new mongoose.Schema({
+	avatar: {
+		type: Buffer,
+		required: true,
+	},
+	games: {
+		type: [Number],
+		required: true,
+	},
+	lists: {
+		type: [listSchema],
+		required: true,
+	},
+	following: {
+		type: [String],
+		required: true,
+	},
+	followers: {
+		type: [String],
+		required: true,
+	},
+});
 
 const userSchema = new mongoose.Schema({
 	_id: {
@@ -12,6 +60,10 @@ const userSchema = new mongoose.Schema({
 	},
 	passwordHash: {
 		type: String,
+		required: true,
+	},
+	userData: {
+		type: userDataSchema,
 		required: true,
 	},
 });
