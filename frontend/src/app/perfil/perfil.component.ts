c1140878b354b30430c038c8b9208d3df6da65fa
corrreia <tomasc2002@gmail.com>
2023-05-18 02:03:21 +0100

    updated profile

--- frontend/src/app/perfil/perfil.component.ts
+++ frontend/src/app/perfil/perfil.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { UserData } from '../types/user';
 import { AuthService } from '../services/auth.service';
+import { ItemService } from '../services/item.service';
+import { forkJoin, map } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
@@ -12,9 +15,13 @@ export class PerfilComponent {
   username!: string;
   user!: UserData;
 
+  lists: { id: number; name: string }[] = [];
+
   constructor(
     private authService: AuthService,
-    private userService: UserService
+    private userService: UserService,
+    private router: Router,
+    private itemService: ItemService
   ) {
     this.authService.getUser().subscribe((user) => {
       this.username = user.username;
@@ -23,17 +30,32 @@ export class PerfilComponent {
         this.user = user;
       });
     });
+
+    this.getLists().subscribe((list) => {
+      const gameItems = list.map((game) => {
+        return this.itemService.getItem(game).pipe(
+          map((item) => {
+            return { id: item._id, name: item.name };
+          })
+        );
+      });
+      forkJoin(gameItems).subscribe((games) => {
+        this.lists = games;
+      });
+    });
   }
 
-  links = [{ text: 'Library' }, { text: 'Lists' }, { text: 'Social' }];
-  contents = [
-    { text: 'Content for Link 1' },
-    { text: 'Content for Link 2' },
-    { text: `Followers: test  Following: test2` },
-  ];
   activeLinkIndex = 0;
 
   setActiveLinkIndex(index: number) {
     this.activeLinkIndex = index;
+  }
+
+  getLists() {
+    return this.userService.getLists(this.username);
+  }
+
+  goToGamePage(id: number) {
+    this.router.navigate(['/game', id]);
   }
 }
